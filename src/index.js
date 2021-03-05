d4985007327de6211cfae21660b9d08bd78b05e6
Sasha Good aka icobra <eiroglif@mail.ru>
2021-03-05 16:16:22 +0300

    feat: add function min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof array == "undefined" || array.length == 0)
+  {
+    return 0;
+  }
+  return Math.min.apply(0,array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array == "undefined" || array.length == 0)
+  {
+    return 0;
+  }
+  return Math.max.apply(0,array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof array == "undefined" || array.length == 0){
+    return 0;
+  }
+  let average = 0;
+  for(x=0; x < array.length; x++){
+     average += array[x];
+  }
+  return average/array.length;
 }
